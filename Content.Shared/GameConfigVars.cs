0cbb4a7c88a5cdffd9ac71f99c75156ab3ea9eb8
moonheart08 <moonheart08@users.noreply.github.com>
2023-05-11 06:56:30 -0500

    that's the trick you dont need that.

--- Content.Shared/GameConfigVars.cs
+++ Content.Shared/GameConfigVars.cs
@@ -6,7 +6,7 @@ namespace Content.Shared;
 // DEVNOTE: This is the same as SS14's CCVars. Except it's not named CCVars as that name is 
 // hot garbage.
 [CVarDefs]
-public sealed class GameConfigVars: CVars
+public sealed class GameConfigVars : CVars
 {
     // Declare persistent game config variables here.
     // ```
@@ -14,7 +14,4 @@ public sealed class GameConfigVars: CVars
     //     VariableName = CVarDef.Create("namespace.varname", default_value, CVar.TYPE | CVar.OTHERTYPE)
     // ``` 
     // This is a good spot to store your database config, among other things.
-
-    public static readonly CVarDef<bool>
-        DummyCVarForTemplate = CVarDef.Create("dummy.whydoineedthis", true, CVar.ARCHIVE);
 }
